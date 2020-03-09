66f126fc0092aec89ca6d19263436c0b02c812db
Rhian Anthony <rhian.anthony@gmail.com>
2020-03-09 12:01:04 -0400

    Update render_home.sh

--- render_home.sh
+++ render_home.sh
@@ -19,12 +19,13 @@ render_section(){
       fi
 
       echo "Title: ${title}"
-
-      scrubbed_title="${title//_/ }"
+      echo "New Title: ${title//_/ }"
+      
+      # scrubbed_title="${title//_/ }"
 
       case "$title" in 
         *_home)
-          printf "  - [%s](%s/%s)\n" "${scrubbed_title}" "${repo}" "${title}" >> "${destination}".md
+          printf "  - [%s](%s/%s)\n" "${title}" "${repo}" "${title}" >> "${destination}".md
           ;;
       esac
 
